fa26beda61f1a512dbc14afb76f84878f587edbb
Owlynn <contact@jessalynn.fr>
2022-06-21 13:07:43 +0200

    creating app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import Searchbar from '../Searchbar/Searchbar'
+import { render } from '@testing-library/react';
+
+
+
+// import logo from './logo.svg';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <Searchbar />
+        <BusinessList />
     </div>
-  );
+    )
+  };
 }
 
 export default App;
